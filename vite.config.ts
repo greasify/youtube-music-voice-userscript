b7436e60555fe01236fc4ec2b94e20d5b2bd53d1
Vitalij Ryndin <crashmax.dev@gmail.com>
2026-09-13 19:13:39 +0300

    chore: enable autoMetaUrls

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [
     userscript({
       entry: 'src/index.ts',
+      autoMetaUrls: true,
       header: {
         name: pkg.name,
         version: pkg.version,
         description: 'Voice control for YouTube Music: next, previous, pause, play, volume.',
+        homepage: 'https://greasify.github.io/youtube-music-voice-userscript/',
         match: 'https://music.youtube.com/*',
         noframes: true,
       },
